e9e5880bc7b36d665d067f954f1da4658b05a840
Brad Hall <bhall228@gmail.com>
2022-03-11 01:24:08 -0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { FavoritePlacesComponent } from './favorite-places/favorite-places.component';
+
+import { PlacesService } from './services/places.service';
+import { PlaceFormComponent } from './place-form/place-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FavoritePlacesComponent,
+    PlaceFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [PlacesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
